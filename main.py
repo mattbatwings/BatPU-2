fe925463578a6ce340c5a9336b5da271a468f94d
mattbatwings <mfpalazzolo@gmail.com>
2024-05-02 10:42:08 -0400

    command line args

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from assembler import assemble
-# from simulator import simulate
+import sys
+
 
 def main():
-    program = 'test'
-
-    assemble(f'programs/{program}.as', 'output.mc')
-    # simulate('output.mc')
+    if len(sys.argv) < 2:
+        exit("Not enough arguments.")
+    
+    assemble(sys.argv[1], sys.argv[2] if len(sys.argv) >= 3 else 'output.mc')
 
 
 if __name__ == '__main__':
