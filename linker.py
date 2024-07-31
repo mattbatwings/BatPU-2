bc9cf926949ae3f6718d846330be35c0ce098a51
Noname-Official <106477298+Noname-Official@users.noreply.github.com>
2024-07-31 21:34:26 +0200

    Use argparse and output to .bin file

--- linker.py
+++ linker.py
@@ -1,7 +1,7 @@
 from typing import Dict
 
 
-def link(object_file_contents: str) -> str:
+def link(object_file_contents: str) -> bytes:
     pc = 0
     labels: Dict[str, int] = {}
     for line in object_file_contents.split("\n"):
@@ -15,7 +15,7 @@ def link(object_file_contents: str) -> str:
         else:
             pc += 1
     
-    byte_code = ""
+    byte_code = b""
     for line in object_file_contents.split("\n"):
         line = line.strip()
         if not line:
@@ -23,13 +23,13 @@ def link(object_file_contents: str) -> str:
         if line[0] == ".":
             continue
         words = line.split(" ")
-        new_words = []
+        new_words = "0b"
         for word in words:
             if word[0] == ".":
                 length = int(word.split("(")[-1][:-1])
                 word = word.rsplit("(", 1)[0]
-                new_words.append(bin(labels[word])[2:].zfill(length))
+                new_words += bin(labels[word])[2:].zfill(length)
             else:
-                new_words.append(word)
-        byte_code += "".join(new_words) + "\n"
+                new_words += word
+        byte_code += bytes.fromhex(hex(int(new_words, 2))[2:])
     return byte_code.strip()
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import argparse
 import os
-import sys
+from typing import List, TextIO, BinaryIO
 
 from assembler import Assembler
 from linker import link
@@ -8,24 +9,34 @@ from tokens import Tokenizer
 
 
 def main():
-    if len(sys.argv) < 2:
-        exit("Not enough arguments.")
+    arg_parser = argparse.ArgumentParser("BatPuAsm", description="BatPU Assembler and Linker by NoName_Official")
+    arg_parser.add_argument("-l", "--link", action="store_true", help="Link instead of assemble")
+    arg_parser.add_argument("-o", "--output", type=argparse.FileType("wb"), help="Output file")
+    arg_parser.add_argument("files", nargs="+", type=argparse.FileType("r", encoding="utf-8"), help="The list of files to assemble or link")
+    args = arg_parser.parse_args()
+    link_file: bool = args.link
+    output_file: BinaryIO = args.output
+    files: List[TextIO] = args.files
     
-    cur_dir = os.path.dirname(sys.argv[0])
-    with open(os.path.join(cur_dir, "std.as"), "r") as f:
-        std = f.read()
-    with open(sys.argv[1], "r") as f:
-        lines = f.read()
+    output_file = output_file or open("output.bin" if link_file else "output.o", "wb", encoding="utf-8")
+    cur_dir = os.path.dirname(__file__)
+    if not link_file:
+        files.insert(0, open(os.path.join(cur_dir, "std.as"), "r", encoding="utf-8"))
     
-    lines = std + lines
+    lines = ""
+    for file in files:
+        with file:
+            lines += file.read()
     
-    tokens = Tokenizer(lines).tokenize()
-    ast = Parser(tokens).parse()
-    object_file_contents = Assembler().visit_program(ast)
-    byte_code = link(object_file_contents)
+    if not link_file:
+        tokens = Tokenizer(lines).tokenize()
+        ast = Parser(tokens).parse()
+        output = Assembler().visit_program(ast).encode("utf-8")
+    else:
+        output = link(lines)
     
-    with open(sys.argv[2] if len(sys.argv) >= 3 else "output.mc", "w") as f:
-        f.write(byte_code)
+    with output_file:
+        output_file.write(output)
 
 
 if __name__ == '__main__':
