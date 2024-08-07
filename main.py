fecf772fbb2ea39c8c7b5318e91d2a788d403169
mattbatwings <mfpalazzolo@gmail.com>
2024-08-07 14:37:47 -0400

    better reset

--- main.py
+++ main.py
@@ -4,10 +4,18 @@ import sys
 
 
 def main():
-    program = 'test'
+    program = 'mandelbrot'
+    
+    as_filename = f'programs/{program}.as'
+    mc_filename = f'programs/{program}.mc'
+    schem_filename = f'programs/{program}.schem'
 
-    assemble(f'programs/{program}.as', f'programs/{program}.mc')
-    make_schematic(f'programs/{program}.mc', f'programs/{program}.schem')
+    assemble(as_filename, mc_filename)
+    make_schematic(mc_filename, schem_filename)
 
 if __name__ == '__main__':
     main()
+
+
+# cpusnake.schem - GOOD
+# cputetris.schem - GOOD
--- schematic.py
+++ schematic.py
@@ -70,6 +70,100 @@ def make_schematic(mc_filename, schem_filename):
         schem.setBlock(tuple(pos), 'minecraft:repeater[facing=north,locked=true,powered=false]')
         pos[1] -= 2
 
+    # Reset call stack
+
+    push_start_pos = [-9, -1, -22]
+    pull_start_pos = [-8, -1, -21]
+    
+    for i in range(16):
+        pos = push_start_pos.copy()
+        pos[2] -= i * 3
+        for _ in range(10):
+            schem.setBlock(tuple(pos), 'minecraft:repeater[facing=south,locked=true,powered=false]')
+            pos[1] -= 2
+
+    for i in range(16):
+        pos = pull_start_pos.copy()
+        pos[2] -= i * 3
+        for _ in range(10):
+            schem.setBlock(tuple(pos), 'minecraft:repeater[facing=north,locked=true,powered=false]')
+            pos[1] -= 2
+
+    # Reset flags
+
+    flag_start_pos = [-26, -17, -60]
+    pos = flag_start_pos.copy()
+
+    schem.setBlock(tuple(pos), 'minecraft:repeater[facing=west,locked=true,powered=false]')
+    pos[2] -= 4
+    schem.setBlock(tuple(pos), 'minecraft:repeater[facing=west,locked=true,powered=false]')
+
+    # Reset data mem
+
+    data_start_pos = [-47, -3, -9]
+    pos_list_north = []
+    
+    for i in range(4):
+        pos = data_start_pos.copy()
+        pos[2] -= 16 * i
+        for j in range(16):
+            pos_list_north.append(pos.copy())
+            pos[0] -= 2
+            if j % 2 == 0:
+                pos[1] += 1
+            else:
+                pos[1] -= 1
+        
+        pos = data_start_pos.copy()
+        pos[2] -= 16 * i
+        pos[0] -= 36
+        pos[1] += 1
+        for j in range(16):
+            pos_list_north.append(pos.copy())
+            pos[0] -= 2
+            if j % 2 == 0:
+                pos[1] -= 1
+            else:
+                pos[1] += 1
+
+    for pos in pos_list_north[:-3]:
+        x = pos.copy()
+        for _ in range(8):
+            schem.setBlock(tuple(x), 'minecraft:repeater[facing=north,locked=true,powered=false]')
+            x[1] -= 2
+
+    for pos in pos_list_north:
+        x = pos.copy()
+        x[2] -= 2
+        for _ in range(8):
+            schem.setBlock(tuple(x), 'minecraft:repeater[facing=south,locked=true,powered=false]')
+            x[1] -= 2
+    
+    # Reset registers
+
+    reg_start_pos = [-35, -3, -12]
+    pos_list_east = []
+
+    pos = reg_start_pos.copy()
+    for i in range(15):
+        pos_list_east.append(pos.copy())
+        pos[2] -= 2
+        if i % 2 == 0:
+            pos[1] -= 1
+        else:
+            pos[1] += 1
+
+    for pos in pos_list_east:
+        x = pos.copy()
+        for _ in range(8):
+            schem.setBlock(tuple(x), 'minecraft:repeater[facing=east,locked=true,powered=false]')
+            x[1] -= 2
+
+        x = pos.copy()
+        x[0] += 2
+        for _ in range(8):
+            schem.setBlock(tuple(x), 'minecraft:repeater[facing=west,locked=true,powered=false]')
+            x[1] -= 2
 
     # Save
 
