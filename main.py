9c3edc49ee810078e91029b6380602bd733cdb29
mattbatwings <mfpalazzolo@gmail.com>
2024-08-17 01:50:09 -0400

    helloworld

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from assembler import assemble
 from schematic import make_schematic
 
 def main():
-    program = 'mandelbrot'
+    program = 'helloworld'
     
     as_filename = f'programs/{program}.as'
     mc_filename = f'programs/{program}.mc'
-    schem_filename = f'programs/{program}.schem'
+    schem_filename = f'programs/{program}program.schem'
 
     assemble(as_filename, mc_filename)
     make_schematic(mc_filename, schem_filename)
